c2efef8569232b5f4001a58a02fb7918b92bf3c7
David Gaitan <jdavid.gaitan@gmail.com>
2019-09-20 21:58:07 -0600

    initial templates

--- views/custom-statuses.php
+++ views/custom-statuses.php
@@ -1,1 +1,9 @@
-<h1>Custom Statuses</h1>
+<div class="wrap woonder-orders" id="pk-custom-status">
+	<h1 class="wp-heading-inline">Custom Status</h1>
+	<a href="#" class="page-title-action">
+		<?php echo __( 'Add Custom Status', $plugin_name ); ?>
+	</a>
+	<hr class="wp-header-end" />
+    <div class="foo">{{ foo }}</div>
+	<!-- /woonder orders -->
+</div>
---
+++ views/settings.php
@@ -0,0 +1,6 @@
+<div class="wrap woonder-orders" id="pk-custom-status">
+	<h1 class="wp-heading-inline">Woonder Orders Settings</h1>
+	<hr class="wp-header-end" />
+    <div class="foo">{{ foo }}</div>
+	<!-- /woonder orders -->
+</div>
