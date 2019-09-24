2459edad728b582364430a936837ba82fb5af8b7
David Gaitan <jdavid.gaitan@gmail.com>
2019-09-24 10:57:56 -0600

    change status register logic to custom status model

--- src/models/CustomStatus.php
+++ src/models/CustomStatus.php
@@ -50,12 +50,59 @@ class CustomStatus extends AbstractModel {
 		parent::__construct( 'pk_custom_status', $fields );
 	}
 
-	public function register_custom_statuses(  ) {
-
+	/**
+	 * Register Custom Status created by the user
+	 * to WooCommerce Status
+	 *
+	 * @since  1.0.0
+	 * @return void
+	 */
+	public function register_custom_status_to_woocommerce() {
+		$this->where( array( 'posts_per_page' => -1 ) );
+		$this->action( 'init', 'register_custom_post_status' );
+		$this->filter( 'wc_order_statuses', 'register_custom_status_to_woo' );
 	}
 
-	public function get_all_orders() {
+	/**
+	 * Register custom status as post status
+	 *
+	 * @since  1.0.0
+	 * @return void
+	 */
+	public function register_custom_post_status() {
+		if ( $this->items ) {
+			foreach ( $this->items as $status ) {
+				$status_slug = "wc-{$status['post_name']}";
+				$status_label = $status['pk_custom_status_name'];
 
+				register_post_status( $status_slug, array(
+			        'label'                     => $status_label,
+			        'public'                    => true,
+			        'exclude_from_search'       => false,
+			        'show_in_admin_all_list'    => true,
+			        'show_in_admin_status_list' => true,
+			        'label_count'               => _n_noop( "{$status_label} <span class='count'>(%s)</span>", "{$status_label} <span class='count'>(%s)</span>" )
+			    ) );
+			}
+		}
+	}
+
+	/**
+	 * Register custom status to woocommerce status
+	 *
+	 * @since  1.0.0
+	 * @param  array $statuses - WooCommerce Statuses
+	 * @return array $statuses - WooCommerce Statuses + Custom Statuses
+	 */
+	public function register_custom_status_to_woo( $statuses ) {
+		if ( $this->items ) {
+			foreach ( $this->items as $status ) {
+				$status_slug = "wc-{$status['post_name']}";
+				$statuses[$status_slug] = _x( $status['pk_custom_status_name'], 'Order Status', 'woocommerce' );
+			}
+		}
+
+		return $statuses;
 	}
 
 }
